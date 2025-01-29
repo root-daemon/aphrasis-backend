815da9bcd3ce27bb5e9010f22b56e24b0ad88b86
HARSHAN A M <harshanmathi06@gmail.com>
2025-01-29 21:33:05 +0530

    feat: users progress endpoint

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from routes.transcribe import transcribe_router
 from routes.user import user_router
+from routes.progress import progress_router
 
 app = FastAPI()
 
 app.include_router(transcribe_router,prefix="/api",tags=["Transcribe"])
 app.include_router(user_router,prefix="/api",tags=["User"])
+app.include_router(progress_router,prefix="/api",tags=["Levels"])
 
 @app.get("/")
 def main():
