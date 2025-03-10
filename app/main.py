ab8532e22476e04c8969980af9e0a29863cb9853
root-daemon <srivishal.sivasubramanian@gmail.com>
2025-03-10 09:39:58 +0530

    fix: cors

--- app/main.py
+++ app/main.py
@@ -9,7 +9,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://aphrasis-blush.vercel.app/"],
+    allow_origins=["http://localhost:3000", "https://aphrasis-blush.vercel.app/", 'https://cron-job.org',
+      'https://cron-job.org/cron-job-scheduler-online/'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
