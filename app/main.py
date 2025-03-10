a12701836efb3b8a19ebad052a15b96e722d90b8
root-daemon <srivishal.sivasubramanian@gmail.com>
2025-03-10 12:10:00 +0530

    fix: cors

--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes.transcribe import transcribe_router
 from app.routes.user import user_router
 from app.routes.levels import level_router
 from app.routes.history import history_router
 
 app = FastAPI()
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(transcribe_router,prefix="/api",tags=["Transcribe"])
 app.include_router(user_router,prefix="/api",tags=["User"])
