8e0364496767a58088cfba33065f1044f0eb0830
HARSHAN A M <harshanmathi06@gmail.com>
2025-03-10 09:45:20 +0530

    feat: user data retrieval endpoint

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,12 +1,22 @@
 from fastapi import APIRouter
 from schemas.user_schema import UserSchema
+from database.supabase_utils import supabase
 
 user_router = APIRouter()
 
 
 @user_router.get("/user/{uuid}")
 def user(uuid: str):
-    return {"message":"Transcribe Route"}
+    try:
+        response = supabase.table("users").select("name","streak","levels").eq("uuid", uuid).execute()
+        
+        if not response.data:
+            raise HTTPException(status_code=404, detail="User not found")
+        return response.data[0]
+        
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error fetching user: {str(e)}")
+
 
 @user_router.post("/user")
 def new_user(data: UserSchema):
