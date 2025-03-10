71d1482f3eea1f7a4baed917970f98500fbf2321
HARSHAN A M <harshanmathi06@gmail.com>
2025-03-10 10:43:12 +0530

    chore: modified column name

--- app/routes/user.py
+++ app/routes/user.py
@@ -8,12 +8,13 @@ user_router = APIRouter()
 @user_router.get("/user/{uuid}")
 def user(uuid: str):
     try:
-        response = supabase.table("users").select("name","streak","levels").eq("uuid", uuid).execute()
+        response = supabase.table("users").select("name","streak","level_completed").eq("uuid", uuid).execute()
         
         if not response.data:
             raise HTTPException(status_code=404, detail="User not found")
+
         return response.data[0]
-        
+
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error fetching user: {str(e)}")
 
